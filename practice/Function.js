3b98f08462528c7c4944084e6037fc0a715adda6
Rameshvlp <rameshvlp2001@gmail.com>
2024-01-07 22:36:00 +0530

    Functions in js

---
+++ practice/Function.js
@@ -0,0 +1,13 @@
+var n=9;
+function num(n){
+var ans= n*n;
+return ans;
+}
+var square=num(9);
+console.log(square);
+var num=5;
+for(i=1;i<=num;i++){
+    var ans=num*i;
+    console.log(ans);
+}
+
