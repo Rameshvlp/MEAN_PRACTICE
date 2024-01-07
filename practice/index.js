1cd849b7bf64be36fb68aeef92bbde65213b1aa3
Rameshvlp <rameshvlp2001@gmail.com>
2024-01-07 21:34:55 +0530

    learning commands

--- practice/index.js
+++ practice/index.js
@@ -4,12 +4,12 @@ client.connect();
 
 
 const database = client.db("mydbs2");
-// const collection = database.collection("phones");
-// // collection.insertOne({model:"samsung", price:"12000", Ram:"4GB"});
-// collection.deleteOne({model:"samsung"});
-// const collection=database.collection("laptops");
-// collection.insertOne({model:"Asus", price:"210000"});
-// collection.insertMany([{ model:"acer", price: "21000" }, {model:"Lenovo", price:"31000"}])
+const collection = database.collection("phones");
+collection.insertOne({model:"samsung", price:"12000", Ram:"4GB"});
+collection.deleteOne({model:"samsung"});
+const collection1=database.collection("laptops");
+collection1.insertOne({model:"Asus", price:"210000"});
+collection.insertMany([{ model:"acer", price: "21000" }, {model:"Lenovo", price:"31000"}])
 const collections=database.collection("bikes");
 collections.insertOne({model:"RoyalEnfield", price:"120000"})
 
